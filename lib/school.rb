adc2739e24f050d459b37aa61d0b803059b11451
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-12 22:04:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,8 +5,7 @@ class School
   school.add_student("Zach Morris", 9)
   school.roster{9 => ["Zach Morris"]}
   school.grade()
-#   hash["new_key"] = []
-# hash["new_key"] << "new_value_for_value_array"
-#
-# hash
-#  => {"new_key"=>["new_value_for_value_array"]}
+  school.sort
+  hash["new_key"] = []
+  hash["new_key"] << "new_value_for_value_array"
+  hash => {"new_key"=>["new_value_for_value_array"]}
