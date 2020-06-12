e72bcc16e193ca61872211e1f10684f548fa7bd2
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-12 22:03:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  def initialize(name)
+  end
+  school = School.new("Bayside High School")
+  school.add_student("Zach Morris", 9)
+  school.roster{9 => ["Zach Morris"]}
+  school.grade()
+#   hash["new_key"] = []
+# hash["new_key"] << "new_value_for_value_array"
+#
+# hash
+#  => {"new_key"=>["new_value_for_value_array"]}
